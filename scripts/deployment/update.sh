554cc4c9f190e6d44c4e92116df77ffb66fe345d
Mark Lewis <thnkloud9@gmail.com>
2016-01-25 21:53:04 +0100

    chore(deployment): use bash instead of Makefile for updates

---
+++ scripts/deployment/update.sh
@@ -0,0 +1,13 @@
+#1/bin/bash
+
+unset GIT_WORK_TREE
+
+BASEDIR=$(dirname $0)
+
+cd $BASEDIR/server
+./scripts/virtual-env-setup.sh
+
+cd $BASEDIR/client/master
+npm install
+bower install
+gulp build
