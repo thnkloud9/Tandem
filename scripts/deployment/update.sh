f1fccb9eb248184a559d918b784b536ebce11cc9
Mark Lewis <thnkloud9@gmail.com>
2016-01-25 22:49:20 +0100

    chore(deployment): configure prod settings for client in update.sh

--- scripts/deployment/update.sh
+++ scripts/deployment/update.sh
@@ -7,6 +7,9 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR/../../server
 ./scripts/virtual-env-setup.sh
 
+# update config for client
+sed 's/tandem\.dev/tandem\.marklewis\.me/g' $BASEDIR/../../client/master/js/modules/core/core.constants.js
+
 cd $BASEDIR/../../client/master
 npm install
 bower install
