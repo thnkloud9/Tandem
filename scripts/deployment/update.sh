9ea7ab1f8b2f59c03e6f47a52ba6ed2be3e4bad3
Mark Lewis <thnkloud9@gmail.com>
2016-01-25 23:10:50 +0100

    chore(deployment): add upstart scripts and update update.sh

--- scripts/deployment/update.sh
+++ scripts/deployment/update.sh
@@ -7,8 +7,9 @@ BASEDIR=$(dirname $0)
 cd $BASEDIR/../../server
 ./scripts/virtual-env-setup.sh
 
+# TODO: find a better solution than this
 # update config for client
-sed 's/tandem\.dev/tandem\.marklewis\.me/g' $BASEDIR/../../client/master/js/modules/core/core.constants.js
+sed -i 's/tandem\.dev/tandem\.marklewis\.me/g' $BASEDIR/../../client/master/js/modules/core/core.constants.js
 
 cd $BASEDIR/../../client/master
 npm install
